de39544ca938e6baf92c0a07f11e81935071a7f2
itonse <govl3dnjs@gmail.com>
2023-05-30 12:16:33 +0900

    프로젝트 생성

---
+++ src/main/java/com/itonse/tableup/TableUpApplication.java
@@ -0,0 +1,13 @@
+package com.itonse.tableup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TableUpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itonse/tableup/TableUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itonse.tableup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableUpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
